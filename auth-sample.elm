942bb3cd30ec9bee1581c895f0b72b54460391cf
Justin Love <git@JustinLove.name>
2017-12-01 08:11:24 -0600

    remove auth sample comment from code

---
+++ auth-sample.elm
@@ -0,0 +1,3 @@
+let _ = Debug.log "auth" (OBSWebSocket.authenticate "supersecretpassword" "PZVbYpvAnZut2SS6JNJytDm9" "ztTBnnuqrqaKDzRM3xcVdbYm") in
+-- hash Ln68W1UNXYyY7xDwp+h5foYLI6bzI1qZjKokTa5ZdwE=
+-- auth zZgWipvwSGrw748kHN4gNpBC1IaeiiWX3Hjkrm849Sc=
--- src/OBSMicCheck.elm
+++ src/OBSMicCheck.elm
@@ -37,9 +37,6 @@ type alias Model =
 
 init : (Model, Cmd Msg)
 init =
---let _ = Debug.log "auth" (OBSWebSocket.authenticate "supersecretpassword" "PZVbYpvAnZut2SS6JNJytDm9" "ztTBnnuqrqaKDzRM3xcVdbYm") in
--- hash Ln68W1UNXYyY7xDwp+h5foYLI6bzI1qZjKokTa5ZdwE=
--- auth zZgWipvwSGrw748kHN4gNpBC1IaeiiWX3Hjkrm849Sc=
   (Model NotConnected "password" { name = "-", sources = []}, Cmd.none)
 
 -- UPDATE
