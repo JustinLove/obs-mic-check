7700e19926f737f5fbe1e5ec40584fbba14da516
Justin Love <git@JustinLove.name>
2017-12-11 09:34:21 -0600

    stream over 5 minutes

--- src/OBSMicCheck.elm
+++ src/OBSMicCheck.elm
@@ -75,7 +75,7 @@ makeModel =
         (AudioRule (AnyAudio (allMics Live)) 5)
       |> RuleSet.insert 
         (VideoState "Stream over - text" Visible) 
-        (AudioRule (AnyAudio (allMics Live)) 120)
+        (AudioRule (AnyAudio (allMics Live)) (5 * 60))
     )
     defaultAudio
     Silent
