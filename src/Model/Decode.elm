21738139127abfbe511b9e6c9d3e3e8c7ad4a849
Justin Love <git@JustinLove.name>
2017-12-28 11:29:56 -0600

    remove obsolete persitance versions

--- src/Model/Decode.elm
+++ src/Model/Decode.elm
@@ -11,33 +11,11 @@ persistenceModel =
   |> andThen (\version ->
     case version of
       1 -> fail "previous version"
-      2 -> v2
-      3 -> v3
+      2 -> fail "previous version"
+      3 -> fail "previous version"
       4 -> v4
       _ -> v4
     )
-
-v2 : Decoder PersistenceModel
-v2 =
-  map7 PersistenceModel
-    (field "ruleSet" ruleSet)
-    (field "frameSampleWindow" int)
-    (field "frameAlarmLevel" float)
-    (succeed "localhost")
-    (succeed 4444)
-    (succeed True)
-    (succeed True)
-
-v3 : Decoder PersistenceModel
-v3 =
-  map7 PersistenceModel
-    (field "ruleSet" ruleSet)
-    (field "frameSampleWindow" int)
-    (field "frameAlarmLevel" float)
-    (field "obsHost" string)
-    (field "obsPort" int)
-    (succeed True)
-    (succeed True)
 
 v4 : Decoder PersistenceModel
 v4 =
